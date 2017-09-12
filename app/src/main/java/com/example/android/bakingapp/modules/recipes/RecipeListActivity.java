e2cd8c8974eb1040f6efa773a6159619c070f71c
Sendy Halim <sendyhalim93@gmail.com>
2017-09-13 00:24:52 +0700

    Change main activity to recipe list activity

---
+++ app/src/main/java/com/example/android/bakingapp/modules/recipes/RecipeListActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.bakingapp.modules.recipes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android.bakingapp.R;
+
+public class RecipeListActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recipe_list_activity);
+    }
+}
