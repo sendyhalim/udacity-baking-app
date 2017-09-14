66e33c54199ba49871a4bb4413f6b720822b76a3
Sendy Halim <sendyhalim93@gmail.com>
2017-09-14 10:28:23 +0700

    Set the num columns to autofit

--- app/src/main/java/com/example/android/bakingapp/modules/recipes/RecipeListFragment.java
+++ app/src/main/java/com/example/android/bakingapp/modules/recipes/RecipeListFragment.java
@@ -3,21 +3,22 @@ package com.example.android.bakingapp.modules.recipes;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.util.Log;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.GridView;
 
 import com.example.android.bakingapp.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.Unbinder;
+
+import static android.support.v7.widget.RecyclerView.*;
 
 public class RecipeListFragment extends Fragment {
-    @BindView(R.id.recipeListGridView)
-    GridView recipeGridView;
+    @BindView(R.id.recipeRecyclerView)
+    RecyclerView recipeRecyclerView;
 
     public RecipeListFragment() { }
 
@@ -28,14 +29,14 @@ public class RecipeListFragment extends Fragment {
         @Nullable ViewGroup container,
         Bundle savedInstanceState
     ) {
-        final GridView rootView = (GridView) inflater.inflate(R.layout.recipe_list_fragment, container, false);
-        final RecipeListGridViewAdapter adapter = new RecipeListGridViewAdapter(
-                rootView.getContext(),
-                new RecipeListViewModel()
-        );
+        final RecyclerView rootView = (RecyclerView) inflater.inflate(R.layout.recipe_list_fragment, container, false);
+        final RecipeListViewModelInterface viewModel = new RecipeListViewModel();
+        final RecipeRecyclerViewAdapter adapter = new RecipeRecyclerViewAdapter(viewModel);
+        final GridLayoutManager layoutManager = new GridLayoutManager(this.getContext(), 3);
 
         ButterKnife.bind(this, rootView);
         rootView.setAdapter(adapter);
+        rootView.setLayoutManager(layoutManager);
 
         return rootView;
     }
---
+++ app/src/main/java/com/example/android/bakingapp/modules/recipes/RecipeRecyclerViewAdapter.java
@@ -0,0 +1,47 @@
+package com.example.android.bakingapp.modules.recipes;
+
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.android.bakingapp.R;
+
+
+public class RecipeRecyclerViewAdapter extends RecyclerView.Adapter<RecipeRecyclerViewAdapter.ViewHolder> {
+    RecipeListViewModelInterface viewModel;
+
+    public RecipeRecyclerViewAdapter(RecipeListViewModelInterface viewModel) {
+        this.viewModel = viewModel;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View view) {
+            super(view);
+        }
+
+        public void setup(RecipeViewModelInterface viewModel) {
+            Log.i("WAT", "YOOOOOOO");
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return viewModel.count();
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater
+            .from(parent.getContext())
+            .inflate(R.layout.recipe, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.setup(viewModel.get(position));
+    }
+}
