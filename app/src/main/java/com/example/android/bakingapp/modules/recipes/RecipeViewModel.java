850ffaad7363916d0bc0b324279277b3a9a1202d
Sendy Halim <sendyhalim93@gmail.com>
2017-09-17 02:49:28 +0700

    Make ingredients parcelable

--- app/src/main/java/com/example/android/bakingapp/modules/recipes/RecipeViewModel.java
+++ app/src/main/java/com/example/android/bakingapp/modules/recipes/RecipeViewModel.java
@@ -1,18 +1,35 @@
 package com.example.android.bakingapp.modules.recipes;
 
+import com.example.android.bakingapp.modules.ingredients.Ingredient;
+import com.example.android.bakingapp.modules.ingredients.IngredientViewModel;
+import com.example.android.bakingapp.modules.ingredients.IngredientViewModelInterface;
+
 import org.parceler.Parcel;
 import org.parceler.ParcelConstructor;
 
+import java.util.ArrayList;
+
 @Parcel
 public class RecipeViewModel implements RecipeViewModelInterface {
-    public final Recipe recipe;
+    public Recipe recipe;
+    public ArrayList<IngredientViewModel> ingredients;
 
     @ParcelConstructor
     public RecipeViewModel(Recipe recipe) {
         this.recipe = recipe;
+
+        this.ingredients = new ArrayList<>();
+
+        for (Ingredient ingredient: recipe.ingredients) {
+            this.ingredients.add(new IngredientViewModel(ingredient));
+        }
     }
 
     public String getName() {
         return this.recipe.name;
+    }
+
+    public ArrayList<? extends IngredientViewModelInterface> getIngredients() {
+        return this.ingredients;
     }
 }
--- app/src/main/java/com/example/android/bakingapp/modules/recipes/RecipeViewModelInterface.java
+++ app/src/main/java/com/example/android/bakingapp/modules/recipes/RecipeViewModelInterface.java
@@ -1,5 +1,10 @@
 package com.example.android.bakingapp.modules.recipes;
 
+import com.example.android.bakingapp.modules.ingredients.IngredientViewModelInterface;
+
+import java.util.ArrayList;
+
 public interface RecipeViewModelInterface {
     String getName();
+    ArrayList<? extends IngredientViewModelInterface> getIngredients();
 }
